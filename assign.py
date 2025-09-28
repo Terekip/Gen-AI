049694e5e563bcf8a5d524cd2d224c0ac099d16e
Gilbert Terer <teretich92@@gmail.com>
2025-09-28 13:38:56 +0300

    Add assign.jac and assign.py with password generator implementation

---
+++ assign.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def password_generator(length=12):
+    characters= string.ascii_letters + string.digits + string.punctuation
+
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+print(password_generator(12))
---
+++ myfile.py
@@ -0,0 +1,4 @@
+import jaclang
+from assignment_1 import lovejac
+
+print(f'Python is awesome. But,{lovejac()}')
